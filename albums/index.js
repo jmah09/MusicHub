f0ad2e7232e88d4990ea442031f3deebdfcd475b
Rescobvr <jmah@BakaNotNice-MacBook-Pro.local>
2019-07-07 14:06:42 -0700

    added style and reusable header component

--- albums/index.js
+++ albums/index.js
@@ -1,14 +1,17 @@
 //Import a library to help create component
 import React from 'react';
 import {Text, View, AppRegistry} from 'react-native';
-
+import Header from './src/components/Header';
 
 //create a component - an object that displays something on the screen - a component is a javacsript function that returns JSX
 // JSX (looks like html) babel turns JSX into Javascript 
 // JSX is just a mask over normal function calls 
 const App = () => (
-    <Text> Some Text </Text>
-)
+    // we can use props to pass data from parent to child component
+    // pass in property name called headerText that has a value of "Albums"
+    // so that when header is rendered, we will take the props obj from parent, it will have property called headerText and value will be shown in JSX in header file
+    <Header headerText = {'Albums'}/>
+);
 
 //render to device
 // have to register at least one component to application 
--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -1,11 +1,33 @@
 //import libraries to create components 
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 //create a component
 //Header component is a child component of the root 
-const Header = () => {
-    return <Text>albums!!</Text>
+const Header = (props) => {
+    // define the style 
+    const {textStyle, viewStyle} = styles;
+    return(
+        <View style = {viewStyle}>
+            <Text style = {textStyle}>{props.headerText}</Text>
+        </View>
+    )
+}
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2
+    },
+    textStyle: {
+        fontSize: 20
+    }
 }
 
 //make component available to other areas of the app 
